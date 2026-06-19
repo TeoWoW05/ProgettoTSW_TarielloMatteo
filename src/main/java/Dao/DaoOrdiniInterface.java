bbfeb97838419d70f43362282d6a5eb393b5b02a
TeoWoW05 <Matteotar89@gmail.com>
2026-06-19 14:26:42 +0200

    Aggiunta interfaccia DaoOrdini

---
+++ src/main/java/Dao/DaoOrdiniInterface.java
@@ -0,0 +1,14 @@
+package Dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import Model.Ordine;
+
+
+public interface DaoOrdiniInterface {
+	void doSave(Ordine ordine) throws SQLException;
+	Ordine doRetrieveByKey(int id) throws SQLException;
+	ArrayList<Ordine> doRetrieveByUtente(String email) throws SQLException;
+	ArrayList<Ordine> doRetrieveAll() throws SQLException;
+	void updateStato(int id, String stato) throws SQLException;
+}
