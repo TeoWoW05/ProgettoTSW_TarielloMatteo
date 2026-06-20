04789ba49fab852ee4511f780cfb5d252129185e
TeoWoW05 <Matteotar89@gmail.com>
2026-06-20 15:05:34 +0200

    aggiunta Servlet di conferma ordine

---
+++ src/main/java/Control/CheckoutConfermaServlet.java
@@ -0,0 +1,35 @@
+package Control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebServlet("/CheckoutConfermaServlet")
+public class CheckoutConfermaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	HttpSession session = request.getSession(false);
+	
+	if(session == null || session.getAttribute("ordineCompletato") == null) {
+		
+		response.sendRedirect(request.getContextPath() + "/");
+		return;
+		}
+	
+		request.getRequestDispatcher("/WEB-INF/View/ConfermaOrdine.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
